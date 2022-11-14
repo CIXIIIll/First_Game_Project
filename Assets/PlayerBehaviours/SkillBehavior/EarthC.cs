24410b8b44ba3ba6338cfa6fbc12cca55582900a
CIXIIIll <54516713+CIXIIIll@users.noreply.github.com>
2022-11-14 00:11:13 -0600

    4.5/8
    Finish Part of EarthC

---
+++ Assets/PlayerBehaviours/SkillBehavior/EarthC.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthC : MonoBehaviour
+{
+    public float FlightSpeed;
+    public float DestoryDistance;
+    private Rigidbody2D rb2d;
+    private Vector3 vector;
+    private Player player;
+    public int currentTimes;
+    public int totalTimes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimes = 0;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        rb2d = GetComponent<Rigidbody2D>();
+        if (player.faceright)
+        {
+            rb2d.velocity = transform.right * FlightSpeed;
+        }
+        else
+        {
+            rb2d.velocity = -(transform.right * FlightSpeed);
+            transform.localScale = new Vector3(-2.485487f, 1.811549f, 1);
+        }
+        vector = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Distance = (transform.position - vector).sqrMagnitude;
+        if (Distance > DestoryDistance)
+        {
+            StartCoroutine(EndofSkill());
+        }
+    }
+    IEnumerator EndofSkill()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (currentTimes <= totalTimes)
+            {
+                collision.GetComponent<Enemy>().CharacterDamage(player.GetDeamge()*2, 0);
+                currentTimes++;
+            }
+            else {
+                collision.GetComponent<Enemy>().CharacterDamage(player.GetDeamge(), 0);
+            }
+        }
+    }
+}
